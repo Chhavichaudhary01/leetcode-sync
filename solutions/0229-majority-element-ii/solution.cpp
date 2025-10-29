1b7608c70c4595824a6e66dacf0e24328c3666e9
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2025-10-29 18:09:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.5 MB (99.86%)

---
+++ solutions/0229-majority-element-ii/solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        
+        // Step 1: Find two candidates using voting algorithm
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+            else if (count1 == 0) { candidate1 = num; count1 = 1; }
+            else if (count2 == 0) { candidate2 = num; count2 = 1; }
+            else { count1--; count2--; }
+        }
+        
+        // Step 2: Verify candidates (count actual occurrences)
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        // Step 3: Return valid candidates
+        vector<int> result;
+        if (count1 > nums.size() / 3) result.push_back(candidate1);
+        if (count2 > nums.size() / 3 && candidate2 != candidate1) 
+            result.push_back(candidate2);
+        
+        return result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
