7a4c988ddcd7b843c742e2364cfbf0ac913bcbf3
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2025-11-15 18:46:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (99.98%)

---
+++ solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        if (m == 0) return 0;  // empty needle case
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+
+            while (j < m && haystack[i + j] == needle[j]) {
+                j++;
+            }
+
+            if (j == m) return i;   // means full match
+        }
+
+        return -1;  // not found
+    }
+};
+
