34a1da179679ba8b528c88398e674c63e460cf24
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2025-11-18 19:02:33 +0000

    Sync LeetCode submission Runtime - 3 ms (14.50%), Memory - 23.9 MB (55.51%)

---
+++ solutions/0283-move-zeroes/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int n = nums.size();
+       int nonzero=0;
+       for(int i = 0;i<n;i++){
+        if(nums[i]!=0){
+        nums[nonzero++]=nums[i];
+        }
+       }
+       for(int i =nonzero ;i<n;i++){
+        nums[i]=0;
+       }
+
+    }
+
+};
