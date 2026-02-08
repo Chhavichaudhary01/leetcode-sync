911089e4721f0bb49fff43cd0310e8642fbc0024
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-02-08 18:38:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (54.27%)

--- solutions/0007-reverse-integer/solution.cpp
+++ solutions/0007-reverse-integer/solution.cpp
@@ -2,15 +2,15 @@
 
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(int n) {
         int rev = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
-            // Check overflow before multiplying by 10
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
-            rev = rev * 10 + pop;
+        while (n != 0) {
+          int r = n%10;
+          if(rev>INT_MAX/10 || rev<INT_MIN/10){
+            return 0;
+          }
+          rev = rev*10+r;
+          n= n/10;
         }
         return rev;
     }
