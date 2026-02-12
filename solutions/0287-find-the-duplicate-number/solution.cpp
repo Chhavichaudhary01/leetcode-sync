e33bf240c72d827cf3385040c4bc50a2742e4644
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-02-12 18:41:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 65.1 MB (41.49%)

---
+++ solutions/0287-find-the-duplicate-number/solution.cpp
@@ -0,0 +1,41 @@
+//*****NOT OPTIMAL APPROACH****//
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+        
+//         unordered_set<int>s;
+//         for(int val: nums){
+//             if(s.find(val) != s.end()){
+//                 return val;
+//             }
+//             s.insert(val);
+//         }
+//         return -1;
+//     }
+// };
+
+///******OPTIMAL APPROACH(SLOW-FAST POINTER=> DETECT CYCLE IN LINKED LIST)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0],fast = nums[0];
+        do{
+            slow = nums[slow]; // +1
+            fast = nums[nums[fast]];//+2
+        }while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow]; // +1
+            fast = nums[fast];  //+1
+
+
+        }
+
+        return slow;
+        
+        
+    }
+};
