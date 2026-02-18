2ec180b03d82587e5aa78841fb1768e97c2564c2
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-02-18 14:56:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (88.00%)

--- solutions/0021-merge-two-sorted-lists/solution.cpp
+++ solutions/0021-merge-two-sorted-lists/solution.cpp
@@ -8,30 +8,50 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* List1, ListNode* List2) {
+//         ListNode*dummy = new ListNode(0);
+//         ListNode*temp= dummy;
+
+//         while(List1!=NULL && List2!=NULL){
+//             if(List1->val<=List2->val){
+//                 temp->next=List1;
+//                 List1= List1->next;
+
+//             }
+//             else{
+//                 temp->next = List2;
+//                 List2=List2->next;
+//             }
+//             temp = temp->next;
+//         }
+//         if(List1!=NULL){
+//             temp->next = List1;
+//         }
+//         else{
+//             temp->next = List2;
+//         }
+//         return dummy->next;
+//     }
+// };
+  
+///////*******RECURSIVE APPROACH********///////
+
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* List1, ListNode* List2) {
-        ListNode*dummy = new ListNode(0);
-        ListNode*temp= dummy;
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+       if(head1 == NULL || head2 ==NULL){
+        return head1 == NULL? head2 : head1;
+       }
 
-        while(List1!=NULL && List2!=NULL){
-            if(List1->val<=List2->val){
-                temp->next=List1;
-                List1= List1->next;
-
-            }
-            else{
-                temp->next = List2;
-                List2=List2->next;
-            }
-            temp = temp->next;
-        }
-        if(List1!=NULL){
-            temp->next = List1;
-        }
-        else{
-            temp->next = List2;
-        }
-        return dummy->next;
+       if(head1->val <= head2->val){
+        head1->next = mergeTwoLists(head1->next,head2);
+        return head1;
+       } 
+       else{
+        head2->next = mergeTwoLists(head1,head2->next);
+        return head2;
+       }
     }
 };
