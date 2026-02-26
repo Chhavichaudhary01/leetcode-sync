4e2852b7889323d619718c0277f0045b0351909e
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-02-26 18:29:52 +0000

    Sync LeetCode submission Runtime - 10 ms (92.48%), Memory - 41.4 MB (47.13%)

---
+++ solutions/0599-minimum-index-sum-of-two-lists/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string, int> mp;
+
+        // store list1
+        for(int i = 0; i < list1.size(); i++){
+            mp[list1[i]] = i;
+        }
+
+        vector<string> ans;
+        int mini = INT_MAX;
+
+        // check list2
+        for(int j = 0; j < list2.size(); j++){
+            if(mp.find(list2[j]) != mp.end()){
+                int sum = j + mp[list2[j]];
+
+                if(sum < mini){
+                    mini = sum;
+                    ans.clear();
+                    ans.push_back(list2[j]);
+                }
+                else if(sum == mini){
+                    ans.push_back(list2[j]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
