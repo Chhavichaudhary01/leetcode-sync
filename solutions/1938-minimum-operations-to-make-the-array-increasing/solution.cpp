9074df646959575ff73b869dd8f164a558002caa
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-03-20 19:10:08 +0000

    Sync LeetCode submission Runtime - 8 ms (75.87%), Memory - 19.5 MB (62.25%)

---
+++ solutions/1938-minimum-operations-to-make-the-array-increasing/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ops =0;
+        for(int i =1;i<nums.size();i++){
+            if(nums[i]<=nums[i-1]){
+                int needed = nums[i-1]+1;
+                ops+= needed - nums[i];
+                nums[i] = needed;
+            }
+        }
+        return ops;
+        
+    }
+};
