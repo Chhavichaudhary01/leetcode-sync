8a82ad6fceefac862b2d1dc89df73b05b5df2348
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-03-22 15:25:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.1 MB (12.75%)

---
+++ solutions/2015-determine-whether-matrix-can-be-obtained-by-rotation/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    bool isEqual(vector<vector<int>>& mat, vector<vector<int>>& target){
+        int n = mat.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] != target[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    void rotate90(vector<vector<int>>& mat){
+        int n = mat.size();
+        vector<vector<int>> temp(n , vector<int>(n));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                temp[j][n-i-1] = mat[i][j];
+            }
+        }
+        mat = temp;
+    }
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+
+        for(int k=0;k<4;k++){
+            if(isEqual(mat , target)) return true;
+            rotate90(mat);
+        }
+        return false;
+    }
+};
