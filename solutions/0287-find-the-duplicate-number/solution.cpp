939e4c7763176358f35295bb53664e92623c7a60
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-03-24 19:23:48 +0000

    Sync LeetCode submission Runtime - 11 ms (42.80%), Memory - 64.9 MB (99.52%)

--- solutions/0287-find-the-duplicate-number/solution.cpp
+++ solutions/0287-find-the-duplicate-number/solution.cpp
@@ -5,11 +5,11 @@
 //     int findDuplicate(vector<int>& nums) {
         
 //         unordered_set<int>s;
-//         for(int val: nums){
-//             if(s.find(val) != s.end()){
-//                 return val;
+//         for(int i = 0;i<nums.size();i++){
+//             if(s.find(nums[i]) != s.end()){
+//                 return nums[i];
 //             }
-//             s.insert(val);
+//             s.insert(nums[i]);
 //         }
 //         return -1;
 //     }
