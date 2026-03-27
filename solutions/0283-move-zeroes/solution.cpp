ae812de5baa1ce145cf68d0f809006713c6adbb0
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-03-27 09:59:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.9 MB (55.51%)

--- solutions/0283-move-zeroes/solution.cpp
+++ solutions/0283-move-zeroes/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int n = nums.size();
-       int nonzero=0;
-       for(int i = 0;i<n;i++){
-        if(nums[i]!=0){
-        nums[nonzero++]=nums[i];
+        
+        int slow = 0;
+
+        for(int fast = 0; fast < nums.size(); fast++){
+
+            if(nums[fast] != 0){
+                swap(nums[slow], nums[fast]);
+                slow++;
+            }
         }
-       }
-       for(int i =nonzero ;i<n;i++){
-        nums[i]=0;
-       }
-
     }
-
 };
