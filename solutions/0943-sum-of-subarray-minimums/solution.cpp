4ac278f406b80e3e6fc60ebcb80493107560b693
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-06-10 11:35:40 +0000

    Sync LeetCode submission Runtime - 76 ms (47.58%), Memory - 193.4 MB (75.29%)

---
+++ solutions/0943-sum-of-subarray-minimums/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> prev(n), next(n);
+        stack<int> st;
+
+        // Previous Smaller Element
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            prev[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        // Next Smaller Element
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            next[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        long long ans = 0;
+        long long mod = 1e9 + 7;
+
+        for(int i = 0; i < n; i++) {
+            long long left = i - prev[i];
+            long long right = next[i] - i;
+
+            ans = (ans + (arr[i] * left % mod) * right % mod) % mod;
+        }
+
+        return ans;
+    }
+};
