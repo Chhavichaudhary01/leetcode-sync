9bdc35797dd35530f1f321abb94dc79ddd62a244
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-06-17 08:35:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (47.25%)

--- solutions/0232-implement-queue-using-stacks/solution.cpp
+++ solutions/0232-implement-queue-using-stacks/solution.cpp
@@ -3,31 +3,34 @@ public:
     stack<int> s1, s2;
 
     MyQueue() {
-        
+
     }
-    
+
     void push(int x) {
+        while(!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
         s1.push(x);
+
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
-    
+
     int pop() {
-        peek(); // make sure s2 has elements
-        int val = s2.top();
-        s2.pop();
+        int val = s1.top();
+        s1.pop();
         return val;
     }
-    
+
     int peek() {
-        if(s2.empty()) {
-            while(!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-        }
-        return s2.top();
+        return s1.top();
     }
-    
+
     bool empty() {
-        return s1.empty() && s2.empty();
+        return s1.empty();
     }
 };
