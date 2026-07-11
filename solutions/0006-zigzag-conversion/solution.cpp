7e8de007b4d5a7592214def4eb0ba32d5cfcebe0
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-07-11 18:43:34 +0000

    Sync LeetCode submission Runtime - 2 ms (81.20%), Memory - 13.9 MB (79.46%)

---
+++ solutions/0006-zigzag-conversion/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if (numRows == 1 || numRows >= s.size())
+            return s;
+
+        vector<string> rows(numRows);
+
+        int currRow = 0;
+        bool down = true;
+
+        for (char ch : s) {
+
+            rows[currRow] += ch;
+
+            if (currRow == 0)
+                down = true;
+            else if (currRow == numRows - 1)
+                down = false;
+
+            if (down)
+                currRow++;
+            else
+                currRow--;
+        }
+
+        string ans;
+
+        for (auto &row : rows)
+            ans += row;
+
+        return ans;
+    }
+};
