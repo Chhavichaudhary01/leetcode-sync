0b3b10a08136775342184de8cabaab0cfcf4f118
Chhavichaudhary01 <chhavidagur.ch@gmail.com>
2026-07-18 12:06:50 +0000

    Sync LeetCode submission Runtime - 7 ms (61.79%), Memory - 33 MB (61.94%)

---
+++ solutions/0789-kth-largest-element-in-a-stream/solution.cpp
@@ -0,0 +1,24 @@
+class KthLargest {
+public:
+    int K;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+
+        for (int x : nums) {
+            pq.push(x);
+            if (pq.size() > K)
+                pq.pop();
+        }
+    }
+
+    int add(int val) {
+        pq.push(val);
+
+        if (pq.size() > K)
+            pq.pop();
+
+        return pq.top();
+    }
+};
